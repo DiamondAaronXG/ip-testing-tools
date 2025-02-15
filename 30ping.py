489680ee08ce73622e3ed9baf8dfcdd6820033b9
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-02-15 01:12:06 -0800

    Update 30ping.py
    added comments

--- 30ping.py
+++ 30ping.py
@@ -1,3 +1,9 @@
 import subprocess
+
+# Declares variable, asks and scans for user input
+
 ping_ip_Address = input("IP Address to Ping: ")
+
+# Pings the IP address 30 times
+
 subprocess.run(["ping", "-n", "30", ping_ip_Address])
