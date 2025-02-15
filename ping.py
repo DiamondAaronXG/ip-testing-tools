7ec868ace46d6f70a328f4364f251025ef6dbcd9
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2025-02-15 01:10:35 -0800

    Update ping.py
    added comments

--- ping.py
+++ ping.py
@@ -1,3 +1,9 @@
 import subprocess
+
+# Declares variable, asks and scans for user input
+
 ping_ip_Address = input("IP Address to Ping: ")
+
+# Pings the IP address one time
+
 subprocess.run(["ping", "-n", "1", ping_ip_Address])
